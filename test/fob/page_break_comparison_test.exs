5a79bacfcceb722fb5234aaef610783a8a167c5a
Michael Davis <mcarsondavis@gmail.com>
2021-03-04 14:07:00 -0600

    implement Fob.PageBreak.compare/2
    closes #3

---
+++ test/fob/page_break_comparison_test.exs
@@ -0,0 +1,46 @@
+defmodule Fob.PageBreakComparisonTest do
+  use Fob.RepoCase
+
+  @moduledoc """
+  Tests that the Fob.PageBreak.compare/2 function works as expected and respects
+  nils
+  """
+  @moduledoc since: "0.2.0"
+
+  alias Ecto.Multi
+  alias Fob.PageBreak
+
+  setup do
+    [schema: TieBreakerSchema, repo: Fob.Repo]
+  end
+
+  setup c do
+    Multi.new()
+    |> Multi.insert_all(:seeds, c.schema, c.schema.seed())
+    |> c.repo.transaction()
+
+    :ok
+  end
+
+  test "compare/3 says that the start of the dataset comes before the end", c do
+    schema = c.schema
+    query = from t in schema, order_by: [asc_nulls_last: :date, asc: :id]
+    all_records = c.repo.all(query)
+
+    for n <- 1..(length(all_records) - 1) do
+      prior_record = Fob.page_breaks(query, all_records |> Enum.at(n - 1))
+      record = Fob.page_breaks(query, all_records |> Enum.at(n))
+
+      assert PageBreak.compare(prior_record, record, query) == :lt
+    end
+
+    all_records = Enum.reverse(all_records)
+
+    for n <- 1..(length(all_records) - 1) do
+      prior_record = Fob.page_breaks(query, all_records |> Enum.at(n - 1))
+      record = Fob.page_breaks(query, all_records |> Enum.at(n))
+
+      assert PageBreak.compare(prior_record, record, query) == :gt
+    end
+  end
+end
