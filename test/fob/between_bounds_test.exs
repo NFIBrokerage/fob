fc7a04fbc4c21120c2d994527af591ba978a4ced
Michael Davis <michael.davis@nfiindustries.com>
2021-03-04 13:00:48 -0600

    implement between_bounds/3 for getting a range of records (#2)

---
+++ test/fob/between_bounds_test.exs
@@ -0,0 +1,54 @@
+defmodule Fob.BetweenBoundsTest do
+  use Fob.RepoCase
+
+  @moduledoc """
+  Tests that the Fob.between_bounds/3 function works as expected and respects
+  nils
+  """
+  @moduledoc since: "0.2.0"
+
+  alias Ecto.Multi
+  import Fob, only: [between_bounds: 3, page_breaks: 2]
+
+  setup do
+    [schema: TieBreakerSchema, repo: Fob.Repo]
+  end
+
+  setup c do
+    Multi.new()
+    |> Multi.insert_all(:seeds, c.schema, c.schema.seed())
+    |> c.repo.transaction()
+
+    :ok
+  end
+
+  test "between_bounds/3 can return the entire dataset with nil page breaks",
+       c do
+    schema = c.schema
+    query = from t in schema, order_by: [asc_nulls_last: :date, asc: :id]
+    all_records = c.repo.all(query)
+
+    assert between_bounds(query, nil, nil) |> c.repo.all() == all_records
+  end
+
+  test "between_bounds/3 can return the entire dataset with book-end page-breaks",
+       c do
+    schema = c.schema
+    query = from t in schema, order_by: [asc_nulls_last: :date, asc: :id]
+    all_records = c.repo.all(query)
+    start = page_breaks(query, List.first(all_records))
+    stop = page_breaks(query, List.last(all_records))
+
+    assert between_bounds(query, start, stop) |> c.repo.all() == all_records
+  end
+
+  test "between_bounds/3 can return a subset of the entire dataset", c do
+    schema = c.schema
+    query = from t in schema, order_by: [asc_nulls_last: :date, asc: :id]
+    records = c.repo.all(query) |> Enum.slice(_start_at = 3, _count = 5)
+    start = page_breaks(query, List.first(records))
+    stop = page_breaks(query, List.last(records))
+
+    assert between_bounds(query, start, stop) |> c.repo.all() == records
+  end
+end
