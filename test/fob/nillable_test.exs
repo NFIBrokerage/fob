616904f8aee5974ff79df1c9ba0c517800498c76
Paul Monson <4119175+pmonson711@users.noreply.github.com>
2022-03-14 11:05:02 -0500

    Add tests for nillabe complex coverage

--- test/fob/nillable_test.exs
+++ test/fob/nillable_test.exs
@@ -23,30 +23,7 @@ defmodule Fob.NillableTest do
   end
 
   setup c do
-    records =
-      [
-        {~D[2020-02-12], 1},
-        {~D[2020-02-13], 2},
-        {~D[2020-02-14], 3},
-        {~D[2020-02-15], 4},
-        {~D[2020-02-16], 5},
-        # ---
-        {~D[2020-02-17], 6},
-        {nil, nil},
-        {nil, nil},
-        {nil, nil},
-        {nil, nil},
-        # ---
-        {nil, nil},
-        {nil, nil},
-        {nil, nil},
-        {nil, nil},
-        {nil, nil}
-      ]
-      |> Enum.with_index()
-      |> Enum.map(fn {{date, count}, index} ->
-        %{id: index, date: date, count: count}
-      end)
+    records = c.schema.seed()
 
     Multi.new()
     |> Multi.insert_all(:seeds, c.schema, records)
--- test/support/nillable_schema.ex
+++ test/support/nillable_schema.ex
@@ -10,4 +10,30 @@ defmodule NillableSchema do
     field :date, :date
     field :count, :integer
   end
+
+  def seed do
+    [
+      {~D[2020-02-12], 1},
+      {~D[2020-02-13], 2},
+      {~D[2020-02-14], 3},
+      {~D[2020-02-15], 4},
+      {~D[2020-02-16], 5},
+      # ---
+      {~D[2020-02-17], 6},
+      {nil, nil},
+      {nil, nil},
+      {nil, nil},
+      {nil, nil},
+      # ---
+      {nil, nil},
+      {nil, nil},
+      {nil, nil},
+      {nil, nil},
+      {nil, nil}
+    ]
+    |> Enum.with_index()
+    |> Enum.map(fn {{date, count}, index} ->
+      %{id: index, date: date, count: count}
+    end)
+  end
 end
